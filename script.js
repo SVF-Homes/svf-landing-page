48abed8ec9e0ff9b6fb41a7db1935613b24906e2
SVF-Homes <shalinivj412@gmail.com>
2025-06-10 14:00:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,7 @@ function learnMore() {
   window.open("https://www.svfhomes.com");
 }
 
-<script>
-function callNow() {
-  const choice = prompt("Call which number?\n1. +91 9444892265\n2. +91 9876543210");
 
-  if (choice === "1") {
-    window.location.href = "tel:+919444892265";
-  } else if (choice === "2") {
-    window.location.href = "tel:+919876543210";
-  } else {
-    alert("Invalid choice. Please enter 1 or 2.");
-  }
-}
-</script>
+<a href="tel:+919876543210">
+  <button>Call Now</button>
+</a>
